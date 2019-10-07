71c80fbededc5ebaf9ca7a9f8a21aa1aedecaf2c
DEV_YUN <riosee2415@gmail.com>
2019-10-07 15:48:54 +0900

    origin

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from my ass!!");
+
+const handlePrifile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handlePrifile);
+
+app.listen(PORT, handleListening);
