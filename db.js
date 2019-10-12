d70c7677e5e7dcad889332ed704097ddba01632a
DEV_YUN <riosee2415@gmail.com>
2019-10-12 14:59:51 +0900

    Mixins Create

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ export const videoList = [
     title: "Video Awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -16,7 +17,8 @@ export const videoList = [
     title: "Video Nice",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -28,7 +30,8 @@ export const videoList = [
     title: "Video Perfect",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
@@ -40,7 +43,8 @@ export const videoList = [
     title: "Video Sure",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Nicolas",
