189b90429a3c59664e49a013b8ddff28e8896044
DEV_YUN <riosee2415@gmail.com>
2019-10-14 09:49:20 +0900

    Login Checked

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -9,7 +9,7 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "search", searchingBy });
+  res.render("search", { pageTitle: "search", searchingBy, videoList });
 };
 
 export const videos = (req, res) =>
