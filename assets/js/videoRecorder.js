9e58feba1d222c2f7fcb3c0ee21119ea3e068ec1
DEV_YUN <riosee2415@gmail.com>
2019-10-24 16:35:38 +0900

    media part

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,17 +3,30 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  const { data: videoFile } = event;
+
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start Recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
-  //videoRecorder.ondataavailable = handleVideoData;
   videoRecorder.addEventListener("dataavailable", handleVideoData);
-  console.log(videoRecorder);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
