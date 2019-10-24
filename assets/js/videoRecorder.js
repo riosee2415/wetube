dc54ba6fb5ef22cd5f7fb7c5c0617c29cae5d78e
DEV_YUN <riosee2415@gmail.com>
2019-10-24 15:39:40 +0900

    media part

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,21 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = () => {};
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    });
+
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+  } catch (error) {
+    recordBtn.innerHTML = "☹️ Can't record";
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
 
 function init() {
   recordBtn.addEventListener("click", startRecording);
