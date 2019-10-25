7e83e9de34f58dc27b708a60d4b62cbca22ee01f
DEV_YUN <riosee2415@gmail.com>
2019-10-25 22:51:05 +0900

    AWS S3

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,3 +1,5 @@
+import getBlobDuration from "get-blob-duration";
+
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
@@ -92,8 +94,11 @@ function getCurrentTime() {
   currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
 }
 
-function setTotalTime() {
-  const totlaTitmeString = formatDate(videoPlayer.duration);
+async function setTotalTime() {
+  // const blob = await fetch(videoPlayer.src).then(response => response.blob());
+  const duration = await getBlobDuration(videoPlayer.src);
+  console.log(duration);
+  const totlaTitmeString = formatDate(duration);
   totalTime.innerHTML = totlaTitmeString;
   setInterval(getCurrentTime, 1000);
 }
